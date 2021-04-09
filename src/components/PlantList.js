65c83b587bde8fd5a1931b3a1a32c16e3d4cc1b7
Jessica Manak <jessica@Jessicas-MacBook-Air.local>
2021-04-09 07:14:47 -0700

    Finished PlantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,11 @@
-import React, { useState, Component } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+    state = {
+      plants: []
+    }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -12,9 +14,12 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
+        this.setState({
+          plants: res.data,
+        })
       });
   }
+
 
   render() {
     return (
